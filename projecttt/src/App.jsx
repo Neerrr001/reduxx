6d476fa96d970c3324d205fb3621a2c24e1bdc5e
Neerrr001 <neeragarwal711@gmail.com>
2026-04-04 19:17:10 +0530

    Created mediaApi for photos,vid,gifs

--- projecttt/src/App.jsx
+++ projecttt/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import {fetchPhotos, fetchVideos, fetchGifs} from './api/mediaApi'
 const App = () => {
+
   return (
-    <div>
-      App
+    <div className='h-screen w-full bg-gray-900 text-white'>
+      <button className='px-4 py-2 m-4 bg-indigo-500 cursor-pointer active:scale-95 rounded' 
+      onClick={async ()=>{
+        const data = await fetchPhotos('cat')
+        console.log(data.results)
+      }}>Get photos</button>
+
+      <button className='px-4 py-2 m-4 bg-indigo-500 cursor-pointer active:scale-95 rounded'
+      onClick={async ()=>{
+        const data = await fetchVideos('cat')
+        console.log(data.videos)
+      }}>Get Videos</button>
+   
+
+    <button className='px-4 py-2 m-4 bg-indigo-500 cursor-pointer active:scale-95 rounded'
+      onClick={async ()=>{
+        const data = await fetchGifs('cat')
+        console.log(data.data.data)
+      }}>Get GIFS</button>
     </div>
   )
 }
